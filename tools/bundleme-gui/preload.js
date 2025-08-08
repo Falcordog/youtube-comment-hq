905c8cb6195c1cfadcaabf904b04c2f099d52dad
Falcordog <45631069+Falcordog@users.noreply.github.com>
2025-08-09 07:50:24 +1000

    Update preload.js

--- tools/bundleme-gui/preload.js
+++ tools/bundleme-gui/preload.js
@@ -1,7 +1,2 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  chooseDir: () => ipcRenderer.invoke('chooseDir'),
-  openLink: (url) => ipcRenderer.invoke('openLink', url),
-  runBundle: (p) => ipcRenderer.invoke('runBundle', p)
-});
+const { contextBridge } = require('electron');
+contextBridge.exposeInMainWorld('api', { ping: () => 'pong' });
